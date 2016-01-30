91a6cc34839f9f3cef1eefba53121430a06575f2
purgency <quach.philipp@googlemail.com>
2016-01-30 06:15:00 +0100

    corrected deck

--- PS/uno.js
+++ PS/uno.js
@@ -31,11 +31,15 @@ class Uno extends Rooms.RoomGame {
 		}
 		this.playernum = numberusers;
 		
-		this.deck = ["blue.1", "blue.2", "blue.3", "blue.4", "blue.5", "blue.6", "blue.7", "blue.8", "blue.9", "blue.2x", "blue.Invert", "blue.skip", 
+		this.deck = ["blue.0", "blue.1", "blue.2", "blue.3", "blue.4", "blue.5", "blue.6", "blue.7", "blue.8", "blue.9", "blue.2x", "blue.Invert", "blue.skip", 
+		             "red.0", "red.1", "red.2", "red.3", "red.4", "red.5", "red.6", "red.7", "red.8", "red.9", "red.2x", "red.Invert", "red.skip",
+		             "yellow.0", "yellow.1", "yellow.2", "yellow.3", "yellow.4", "yellow.5", "yellow.6", "yellow.7", "yellow.8", "yellow.9", "yellow.2x", "yellow.Invert", "yellow.skip",
+		             "green.0", "green.1", "green.2", "green.3", "green.4", "green.5", "green.6", "green.7", "green.8", "green.9", "green.2x", "green.Invert", "green.skip",
+		             "wish.", "wish.", "wish.", "wish.", "wish.4x", "wish.4x", "wish.4x", "wish.4x",
+		             "blue.1", "blue.2", "blue.3", "blue.4", "blue.5", "blue.6", "blue.7", "blue.8", "blue.9", "blue.2x", "blue.Invert", "blue.skip", 
 		             "red.1", "red.2", "red.3", "red.4", "red.5", "red.6", "red.7", "red.8", "red.9", "red.2x", "red.Invert", "red.skip",
 		             "yellow.1", "yellow.2", "yellow.3", "yellow.4", "yellow.5", "yellow.6", "yellow.7", "yellow.8", "yellow.9", "yellow.2x", "yellow.Invert", "yellow.skip",
-		             "green.1", "green.2", "green.3", "green.4", "green.5", "green.6", "green.7", "green.8", "green.9", "green.2x", "green.Invert", "green.skip",
-		             "wish.", "wish.", "wish.", "wish.", "wish.4x", "wish.4x", "wish.4x", "wish.4x",];
+		             "green.1", "green.2", "green.3", "green.4", "green.5", "green.6", "green.7", "green.8", "green.9", "green.2x", "green.Invert", "green.skip",];
 		this.deck = shufflecards(this.deck);
 		this.playersdeck = new Array(numberusers);
 		for(var i = 0; i < numberusers; i++)
